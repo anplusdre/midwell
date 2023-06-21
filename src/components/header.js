ad6c05461bd062294710752b6e18b68b050ec913
anplusdre <32082154+anplusdre@users.noreply.github.com>
2023-06-21 16:25:10 +0700

    React

---
+++ src/components/header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const MyHeader = () => {
+  return (
+    <div className="header">
+        <div className="container">
+            <a className="logo" href="#"><img src="/assets/midwell_logo.svg"/></a>
+        </div>
+    </div>
+  );
+};
+
+export default MyHeader;
