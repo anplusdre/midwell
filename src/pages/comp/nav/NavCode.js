c84cb3ebcbaa52ff92b1ffadf51e4bdc6b85f2ab
anplusdre <anplusdre@gmail.com>
2023-07-13 18:37:25 +0700

    qwqew

--- src/pages/comp/nav/NavCode.js
+++ src/pages/comp/nav/NavCode.js
@@ -4,6 +4,10 @@ Code1 : `<nav className="nav"></nav>`,
 Code2 : `<nav className="nav">
     <div className="container"></div>
 </nav>`,
+Code3 : `<nav className="nav">
+    <div>Logo</div>
+    <div>Contact</div>
+</nav>`,
 };
 
 export default NavigationCodes;
--- src/pages/comp/nav/Navigation.js
+++ src/pages/comp/nav/Navigation.js
@@ -25,10 +25,22 @@ const NavigationComponent = () => {
                     <h5 className={styles.title}>Get Started</h5>
                     <p>Create a parent element for your navigation using <code>.nav</code>, and wrap all your buttons inside.</p>
                     
-                    <Exemplar buttonContent={<nav className="nav"></nav>} />
+                    {/* <Exemplar buttonContent={<nav className="nav"></nav>} /> */}
                     <CodeBlockComponent text={NavigationCodes.Code1} language="jsx" Copy={true} CodeTitle={"jsx"} />
-                    <p className="top-24 btm-24">or you can use <code>.container</code> inside your <code>.nav</code></p>
+                    <p>or you can use <code>.container</code> inside your <code>.nav</code> to give it's children a fixed width of 1280px</p>
                     <CodeBlockComponent text={NavigationCodes.Code2} language="jsx" Copy={true} CodeTitle={"jsx"} />
+
+                    <p>The height of the navigation bar will depends on it's children's height plus assigned default padding.</p>
+
+                    <Exemplar buttonContent={<nav className="nav">
+                      <div>Logo</div>
+                      <div>Contact</div>
+                    </nav>} />
+
+                    <CodeBlockComponent text={NavigationCodes.Code3} language="jsx" Copy={true} CodeTitle={"jsx"} />
+                    <div className="callout co-warning darkTheme">
+                    At the example above it's total height would be 40px since its content is a text with 16px height from line-height, and the <code>.nav</code> default paddings are 12px.
+                    </div>
                 </div>
 
                 <hr />
